4ac5d0312584158cd2af1eb525ff4ba75d123476
Bhushan Mer <bhushanmer@gmail.com>
2018-10-15 17:49:43 +0530

    login blade is added.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login..</title>
+</head>
+<body>
+	Please Wait... 
+	<div id="test_div" style="display: none;">
+		<img src="http://mem.meditab.test:8010/start/session?token={{$arrUserScopes[1]}}">
+		<img src="http://mos.meditab.test:8020/start/session?token={{$arrUserScopes[2]}}">
+	</div>
+</body>
+</html>
